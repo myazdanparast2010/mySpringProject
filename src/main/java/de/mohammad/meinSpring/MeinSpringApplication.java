b15fcd788487adaff09656d42da575e6391279c8
Mohammad Yazdanparast <m.yazdanparast2010@gmail,com>
2025-05-25 12:15:33 +0200

    init project

---
+++ src/main/java/de/mohammad/meinSpring/MeinSpringApplication.java
@@ -0,0 +1,13 @@
+package de.mohammad.meinSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeinSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeinSpringApplication.class, args);
+	}
+
+}
