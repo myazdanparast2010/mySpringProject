e33086f436adb33710c77eb116eeee28354a19f6
Mohammad Yazdanparast <m.yazdanparast2010@gmail,com>
2025-06-01 16:37:18 +0200

    add springData

---
+++ src/main/java/de/mohammad/meinSpring/entity/Person.java
@@ -0,0 +1,17 @@
+package de.mohammad.meinSpring.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+
+
+}
